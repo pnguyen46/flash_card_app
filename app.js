33d73b1545131d4ac6579afc511b02a3e4fad070
Tron Nguyen <pnguye894@gmail.com>
2022-12-19 13:57:10 -0500

    Add favicon

--- app.js
+++ app.js
@@ -1,37 +1,33 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 7000;
-const bodyParser = require('body-parser');
-const cookieParser = require('cookie-parser');
+const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
 
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 
-app.listen(PORT,() => console.log(`Server running on port ${PORT}`));
+app.set("view engine", "pug");
+// app.set('views',__dirname + '/templates');
 
-app.set('view engine','pug');
-app.set('views',__dirname + '/templates');
-
-app.use(bodyParser.urlencoded({extended:false}));
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use('/static',express.static('public'))
+app.use("/static", express.static("public"));
 
-
-const mainRoute = require('./routes');
-const cardRoute = require('./routes/cards');
-const sandRoute = require('./routes/sandbox')
+const mainRoute = require("./routes");
+const cardRoute = require("./routes/cards");
+const sandRoute = require("./routes/sandbox");
 
 app.use(mainRoute);
-app.use('/cards',cardRoute);
-app.use('/sandbox',sandRoute)
+app.use("/cards", cardRoute);
+app.use("/sandbox", sandRoute);
 
-app.use((req,res,next) => {
-    const error = new Error('Not Found');
-    error.status = 404;
-    next(error);
+app.use((req, res, next) => {
+  const error = new Error("Not Found");
+  error.status = 404;
+  next(error);
 });
 
-app.use((error,req,res,next) => {
-    res.status(error.status);
-    res.render('error',{error});
+app.use((error, req, res, next) => {
+  res.status(error.status);
+  res.render("error", { error });
 });
-
-
